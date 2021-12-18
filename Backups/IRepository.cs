e75eca30816d89fafdca4bdc49238802537a2bd3
d <d>
2021-12-18 15:25:43 +0300

    refactor: update struct

--- Backups/IRepository.cs
+++ Backups/IRepository.cs
@@ -5,6 +5,6 @@ namespace Backups
 {
     public interface IRepository
     {
-        public void CreateStorageZip(List<JobObject> jobObjects, IAlgorithm algorithm, string path, string id, Backupjob backupJob, DateTime dateTime, RestorePoint restorePoint);
+         void CreateStorageZip(List<JobObject> jobObjects, IAlgorithm algorithm, string path, string id, Backupjob backupJob, DateTime dateTime, RestorePoint restorePoint);
     }
 }
